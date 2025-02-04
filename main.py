a628627a4f85001a83fc10e69f53eba81c0ef945
Username387J <jdeleijagbulu@gmail.com>
2025-02-03 20:47:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,52 +2,52 @@ from random import randint
 import pgzrun
 from time import time
 
-WIDTH=300
-HEIGHT=300
-TITLE="CONNECT THE DOTS"
+WIDTH = 600
+HEIGHT = 600
+TITLE= "SPACE GAME"
 
-dots = []
-num_of_dots = 8
-next_dot = 0
+satellites = []
+number_of_satellites = 8
+next_satellite = 0
 lines = []
-game_over=False
+start_time = 0
+total_time = 0
+end_time = 0
 
-
-def createDot():
+def createSatellite():
     global start_time
-    for i in range (0,num_of_dots):
-        dot=Actor("dot")
-        dot.pos= randint(50,250), randint(50,250)
-        dots.append(dot)
+    for i in range (0,number_of_satellites):
+        satellite=Actor("satellite")
+        satellite.pos= randint(50,550), randint(50,550)
+        satellites.append(satellite)
 
-def time_up():
-    global game_over
-    game_over=True 
+    start_time=time()
 
 
 def draw():
 
-    screen.blit("square",(0,0))
+    screen.blit("sky",(0,0))
     num=1
     
-    for dot in dots:
-        dot.draw()
-        screen.draw.text(str(num), (dot.pos[0], dot.pos[1]-3))
+    for satellite in satellites:
+        satellite.draw()
+        screen.draw.text(str(num), (satellite.pos[0], satellite.pos[1]+20))
         num= num+1
 
     for line in lines:
         screen.draw.line(line[0], line[1], (255,255,255))
 
-        if game_over:
-             screen.fill("white")
-             screen.draw.text("Time's Up ! You connected : "+str(next_dot),"dots together", center=(50,50), fontsize=35, color="black")
-    
-       
+    if next_satellite < number_of_satellites:
+        total_time = time() - start_time
+        screen.draw.text(str(round(total_time)), (10,10), )
+    else:
+        screen.draw.text(str(round(total_time)), (10,10), )
 
-
+if next_satellite== 8:
+    screen.draw.text(("Good job"),(300,300), fontsize=55)
 def update():
     pass
 
-clock.schedule(time_up, 60.0)
-createDot()
+
+createSatellite()
 pgzrun.go()
