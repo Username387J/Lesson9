8fb8b2be1ebdb4c0e98f361f899f67a3f96e6581
Username387J <jdeleijagbulu@gmail.com>
2025-03-03 18:17:59 -0700

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,54 +1,69 @@
 from random import randint
 import pgzrun
-from time import time
 
-WIDTH=300
-HEIGHT=300
-TITLE="CONNECT THE DOTS"
 
+WIDTH = 300
+HEIGHT = 300
+TITLE = "CONNECT THE DOTS"
 dots = []
-num_of_dots = 8
+num_of_dots = 20
 next_dot = 0
 lines = []
-game_over=False
+game_over = False
+time_left=60
+time = 30.0
 
 
-def createDot():
-    global start_time
-    for i in range (0,num_of_dots):
-        dot=Actor("dot")
-        dot.pos= randint(50,250), randint(50,250)
+def create_dot():
+    for i in range(0, num_of_dots):
+        dot = Actor("dot")
+        dot.pos = randint(50, 250), randint(50, 250)
         dots.append(dot)
-
-def time_up():
-    global game_over
-    game_over=True 
 
 
 def draw():
-
-    screen.blit("square",(0,0))
-    num=1
-    
+    screen.blit("square", (0, 0))
+    num = 1  
     for dot in dots:
         dot.draw()
-        screen.draw.text(str(num), (dot.pos[0], dot.pos[1]-3))
-        num= num+1
+        screen.draw.text(str(num), (dot.pos[0], dot.pos[1]-7))
+        num += 1  
 
     for line in lines:
-        screen.draw.line(line[0], line[1], (255,255,255))
+        screen.draw.line(line[0], line[1], (0, 255, 255))
 
-        if game_over:
-             screen.fill("white")
-             screen.draw.text("Time's Up ! You connected : "+str(next_dot),"dots together", center=(50,50), fontsize=35, color="black")
-    
-       
+    if next_dot == 20:
+        screen.draw.text("Congratulations", (175, 175), fontsize=30)
 
+    if game_over==True:
+        screen.fill("white")
+        screen.draw.text("Time's Up! You got to dot number {next_dot}", center=(175, 175), fontsize=35, color="black")
+
+    time_left=screen.draw.text(str(), center=(50,50), fontsize=35, color="black")
 
 def update():
     pass
 
-clock.schedule(time_up, 60.0)
-createDot()
-pgzrun.go()
 
+def on_mouse_down(pos):
+    global next_dot, lines
+    if next_dot < num_of_dots:
+        if dots[next_dot].collidepoint(pos):
+            if next_dot:
+                lines.append((dots[next_dot - 1]).pos , dots[next_dot].pos)
+            next_dot = next_dot+1
+        else:
+            lines = []  
+            next_dot = 0  
+
+
+def time_up():
+    global game_over
+    game_over = True
+
+
+
+clock.schedule(time_up,time)
+
+create_dot()
+pgzrun.go()
